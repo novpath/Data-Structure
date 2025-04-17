6a16a2677e632dc1ed8919ba5f6588919882b260
novpath <563959624@qq.com>
2025-04-17 17:26:49 +0800

    :memo: 第 2 章 线性表课外习题下篇完结

---
+++ 代码示例/2.b2.20 RearrangedList.cpp
@@ -0,0 +1,35 @@
+/**
+ * AcWing 3757 Ô­Ìâ-Accepted 
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+typedef struct ListNode LNode;
+ 
+void rearrangedList(LNode* head) {
+    LNode *p = head, *q = p, *r, *s;
+    while (q->next != NULL && q->next->next != NULL) {
+        p = p->next;
+        q = q->next->next;
+    }
+    q = p->next;
+    p->next = NULL;
+    while (q != NULL) {
+        r = q->next;
+        q->next = p->next;
+        p->next = q;
+        q = r;
+    }
+    s = head;
+    q = p->next;
+    p->next = NULL;
+    while (q != NULL) {
+        r = q->next;
+        q->next = s->next;
+        s->next = q;
+        s = q->next;
+        q = r;
+    }
+}
