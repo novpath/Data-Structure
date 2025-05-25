b9a9c9f4b3792b260609eaff983f17773f108d10
novpath <563959624@qq.com>
2025-05-25 17:47:10 +0800

    :memo: 第 5 章 线索二叉树

---
+++ CodeExample/5.3.2 InThread(LocalPre).cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+typedef int ElementType;
+
+/* 线索二叉树的结点 */
+typedef struct ThreadNode {
+    ElementType data;                   // 数据元素
+    struct ThreadNode *lchild, *rchild; // 左、右孩子指针
+    int ltag, rtag;                     // 左、右线索标志
+} ThreadNode, *ThreadTree;
+
+void InThread(ThreadTree p, ThreadTree &pre)
+{
+    if (p != NULL) {
+        InThread(p->lchild, pre);// 递归，线索化左子树
+        if (p->lchild == NULL) {        // 当前结点的左子树为空
+            p->lchild = pre;            // 建立当前结点的前驱线索
+            p->ltag = 1;
+        }
+        if (pre != NULL && pre->rchild == NULL) {  // 前驱结点非空且其右子树为空
+            pre->rchild = p;                       // 建立前驱结点的后继线索
+            pre->rtag = 1;
+        }
+        pre = p;                            // 标记当前结点成为刚刚访问过的结点
+        InThread(p->rchild, pre);    // 递归，线索化右子树
+    }
+}
+
+void CreateInThread(ThreadTree T)
+{
+    if (T != NULL) {                // 非空二叉树，线索化
+        ThreadTree pre = NULL;
+        InThread(T, pre);    // 线索化二叉树
+        pre->rchild = NULL;         // 处理遍历的最后一个结点
+        pre->rtag = 1;
+    }
+}
+
+int main()
+{
+    return 0;
+}
