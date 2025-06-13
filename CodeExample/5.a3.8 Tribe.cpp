7a5a7662ee677a9eb4d67e57ff1e0617ec4ed1f2
novpath <563959624@qq.com>
2025-06-13 15:01:23 +0800

    :memo: 第5章-树与二叉树-a3组基础实验完结

--- CodeExample/5.a3.8 Tribe.cpp
+++ CodeExample/5.a3.8 Tribe.cpp
@@ -1,4 +1,6 @@
 #include <cstdio>
+#define SIZE 10001
+int S[SIZE];
 
 void Union(int S[], int Root1, int Root2)
 {
@@ -25,11 +27,44 @@ int Find(int S[], int x)
     return root;
 }
 
+void InitSet(int S[])
+{
+    for (int i = 0; i < SIZE; i++)
+        S[i] = -1;
+}
+
 int main()
 {
-    int n;
+    int n, k, maxId = 0;
+    int a, b, i, t = -1;
+    InitSet(S);
     scanf("%d", &n);
-    int tirbe[n]
+    for (i = 0; i < n; i++) {
+        scanf("%d", &k);
+        for (int j = 0; j < k; j++) {
+            scanf("%d", &a);
+            if (maxId < a)
+                maxId = a;
+            if (j != 0)
+                Union(S, Find(S, t), Find(S, a - 1));
+            t = a - 1;
+        }
+    }
+    int tribeNum = 0;
+    for (i = 0; i < maxId; i++) {
+        if (S[i] < 0)
+            tribeNum++;
+    }
+    printf("%d %d\n", maxId, tribeNum);
+    int q;
+    scanf("%d", &q);
+    for (i = 0; i < q; i++) {
+        scanf("%d %d", &a, &b);
+        if (Find(S, a - 1) == Find(S, b - 1))
+            printf("Y\n");
+        else
+            printf("N\n");
+    }
 
     return 0;
 }
